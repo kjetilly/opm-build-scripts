a96f80c935d570aeb5facaa3b646acbf9f544f18
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-04-12 11:43:48 +0200

    Enabled ninja

--- scripts/build_opm.sh
+++ scripts/build_opm.sh
@@ -6,6 +6,11 @@ cd build
 cmake .. \
     -DCMAKE_PREFIX_PATH="$(realpath ../../dune);$(realpath ../../zoltan)" \
     -DCMAKE_CXX_COMPILER=$CXX \
-    -DCMAKE_C_COMPILER=$CC
-make -j4
+    -DCMAKE_C_COMPILER=$CC \
+    -GNinja \
+    -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
+    -DCMAKE_CUDA_COMPILER_LAUNCHER=$(which ccache) \
+    -DCMAKE_C_COMPILER_LAUNCHER=$(which ccache) 
+    
+ninja -j4
     
