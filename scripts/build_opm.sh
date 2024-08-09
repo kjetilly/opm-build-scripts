e3ef5897599293ed49b7be3322ec04f15b01abdf
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-08-09 09:42:15 +0200

    Added debug compiling

--- scripts/build_opm.sh
+++ scripts/build_opm.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-set -e 
-mkdir -p build
-cd build
-cmake .. \
-    -DCMAKE_PREFIX_PATH="$(realpath ../../dune);$(realpath ../../zoltan)" \
-    -DCMAKE_CXX_COMPILER=$CXX \
-    -DCMAKE_C_COMPILER=$CC \
-    -GNinja \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
-    -DCMAKE_CUDA_COMPILER_LAUNCHER=$(which ccache) \
-    -DCMAKE_C_COMPILER_LAUNCHER=$(which ccache) 
+for build_type in "Release" "Debug";
+do
+    set -e
+    build_type_lower=$(echo $build_type|awk '{print tolower($0)}')
+    build_folder="build_${build_type_lower}"
+    mkdir -p ${build_folder}
+    cd ${build_folder}
+    cmake .. \
+	  -DCMAKE_PREFIX_PATH="$(realpath ../../dune);$(realpath ../../zoltan)" \
+	  -DCMAKE_CXX_COMPILER=$CXX \
+	  -DCMAKE_C_COMPILER=$CC \
+	  -GNinja \
+	  -DCMAKE_BUILD_TYPE=${build_type} \
+	  -DCMAKE_CXX_COMPILER_LAUNCHER=$(which ccache) \
+	  -DCMAKE_CUDA_COMPILER_LAUNCHER=$(which ccache) \
+	  -DCMAKE_C_COMPILER_LAUNCHER=$(which ccache) 
     
-ninja -j4
-    
+    ninja -j4
+    cd ..
+done
