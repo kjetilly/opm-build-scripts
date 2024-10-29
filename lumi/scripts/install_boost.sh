be6e290ab8d69cf27d7d174d9183f4b80ced2944
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-10-29 10:31:54 +0100

    lumi fixes

--- lumi/scripts/install_boost.sh
+++ lumi/scripts/install_boost.sh
@@ -10,9 +10,9 @@ wget https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83
 tar xf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}_rc2.tar.gz
 cd boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}
 export CXX=$(which CC)
-CC=${CXX} ./bootstrap.sh --with-libraries=program_options,filesystem,system,regex,thread,chrono,date_time,log,test,python --prefix=$INSTALL_PREFIX
-./b2 -d0 --link=static threading=multi --layout=tagged install
-./b2 --link=static threading=multi --layout=tagged install
+./bootstrap.sh --with-libraries=program_options,filesystem,system,regex,thread,chrono,date_time,log,test,python --prefix=$INSTALL_PREFIX
+./b2 -d0 --link=static threading=multi --layout=tagged --cxx=$(which CC) install
+./b2 --link=static threading=multi --layout=tagged --cxx=$(which CC) install
 cd ..
 rm -rf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}
 rm -rf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}_rc2.tar.bz2
