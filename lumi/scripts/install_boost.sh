cabfbb94ba370378be9949d23bcde9c8394a247d
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2024-10-29 10:20:51 +0100

    fixes to boost

--- lumi/scripts/install_boost.sh
+++ lumi/scripts/install_boost.sh
@@ -9,8 +9,8 @@ old_dir=$(pwd)
 wget https://boostorg.jfrog.io/artifactory/main/release/1.83.0/source/boost_1_83_0_rc2.tar.gz
 tar xf boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}_rc2.tar.gz
 cd boost_${BOOST_MAJOR_VERSION}_${BOOST_MINOR_VERSION}_${BOOST_RELEASE_VERSION}
-##CXX=${MY_CXX} ./bootstrap.sh --with-libraries=all --prefix=$INSTALL_PREFIX #program_options,filesystem,system,regex,thread,chrono,date_time,log,spirit --prefix=$INSTALL_PREFIX
- ./bootstrap.sh --with-libraries=program_options,filesystem,system,regex,thread,chrono,date_time,log,test,python --prefix=$INSTALL_PREFIX
+export CXX=$(which CC)
+CC=${CXX} ./bootstrap.sh --with-libraries=program_options,filesystem,system,regex,thread,chrono,date_time,log,test,python --prefix=$INSTALL_PREFIX
 ./b2 -d0 --link=static threading=multi --layout=tagged install
 ./b2 --link=static threading=multi --layout=tagged install
 cd ..
