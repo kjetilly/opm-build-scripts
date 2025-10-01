136bd6101e120660ae569d3a3d6377070070bbbd
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-10-01 11:51:21 +0200

    added hypre build for lumi

--- lumi/scripts/install_mpfr.sh
+++ lumi/scripts/install_mpfr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 install_dir=$(realpath $1)
-wget https://www.mpfr.org/mpfr-current/mpfr-4.2.1.tar.xz
-tar xvf mpfr-4.2.1.tar.xz
-cd mpfr-4.2.1
+wget https://www.mpfr.org/mpfr-current/mpfr-4.2.2.tar.xz
+tar xvf mpfr-4.2.2.tar.xz
+cd mpfr-4.2.2
 ./configure --prefix=${install_dir} --with-gmp=${install_dir}
 make install
